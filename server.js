11b363e5bf2ac467ceca25d4c9b2dc41934a141c
Robert Syvarth <robert@duxter.com>
2013-11-16 07:20:47 -0600

    Reduce transports

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 server.listen( port );
 
+io.set('transports', [
+  //   'websocket'
+  // , 'flashsocket'
+  // , 'htmlfile'
+  'xhr-polling'
+  , 'jsonp-polling'
+]);
+
 //Setup Socket.IO
 io.sockets.on('connection', function (socket) {
 
